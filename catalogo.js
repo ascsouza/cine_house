9473eb08ffa6563c3c658585f65adb6228db36a0
Alisson Souza Campolina <souzaa47@yahoo.com>
2021-07-04 13:38:28 -0300

    adicionando catalogo.js

---
+++ catalogo.js
@@ -0,0 +1,32 @@
+const catalogo = [
+    {
+        codigo: 1,
+        titulo: "Pedro e Maria",
+        duracao: 2.30,
+        atores: ["Tiago","Antonietta"],
+        anoDeLancamento: 2010,
+        emCartaz: true
+    },
+    {
+        codigo: 2,
+        titulo: "The Witcher",
+        duracao: 2.10,
+        atores: ["Marisa", "Tony", "Pedrinho"],
+        anoDeLancamento: 2020,
+        emCartaz: false
+    },
+    {
+        codigo: 3,
+        titulo: "Wachtman",
+        duracao: 1.30,
+        atores: ["Antonio", "Marcela", "Rafa"],
+        anoDeLancamento: 2019,
+        emCartaz: false
+
+        
+    }
+
+   
+];
+
+module.exports = catalogo;
